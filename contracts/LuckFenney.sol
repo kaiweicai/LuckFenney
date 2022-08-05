72ef81f36067cae05249b736b1e7a6be76d33496
kaiweicai <kaiweicai@163.com>
2022-08-06 01:20:09 +0800

    add createLuck

--- contracts/LuckFenney.sol
+++ contracts/LuckFenney.sol
@@ -3,25 +3,39 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
 contract LuckFenney is OwnableUpgradeable {
-
-    uint256 public currentId;
+    uint256 constant QuantityMin = 100;
+    uint256 constant QuantityMax = 10000;
+    uint256 public currentId=0;
+    mapping(address=>uint) public producerLucks;
+    mapping(uint=>Lucky) public runningLucks;
     struct Lucky {
-        address producer;// the project 
+        address producer; // the project
         uint256 id;
         Reward[] rewards;
         uint256 quantity;
         uint256 deadline;
+        LOTTERY_STATE state;
     }
 
-    struct Reward{
+    struct Reward {
         address token;
         RewardType rewardType;
         uint256 amount;
     }
 
-    enum RewardType {ERC20, ERC721,ERC1155 }
+    enum RewardType {
+        ERC20,
+        ERC721,
+        ERC1155
+    }
 
-    event LuckCreated(uint LuckID, address creator);
+    enum LOTTERY_STATE {
+        OPEN,
+        CLOSED,
+        CALCULATING_WINNER
+    }
+
+    event LuckCreated(uint256 LuckID, address creator);
 
     mapping(address => bool) private isManager;
 
@@ -34,7 +48,24 @@ contract LuckFenney is OwnableUpgradeable {
         __Ownable_init();
     }
 
-    function createLuck()public payable {
-
+    function createLuck(Lucky memory luck) public payable {
+        require(luck.rewards.length > 0, "RLBTZ");
+        require(
+            luck.quantity > QuantityMin && luck.quantity <= QuantityMax,
+            "RLBTZ"
+        );
+        require(luck.deadline > block.number, "RLBTZ");
+        //收钱，并且确认收钱的数量。注意weth9的收取。
+        currentId = currentId +1;
+        luck.id = currentId;
+        addRunningLucks(luck);
+        producerLucks[msg.sender] = currentId;
     }
+
+    function addRunningLucks(Lucky memory luck)internal {
+        luck.state = LOTTERY_STATE.OPEN;
+        runningLucks[luck.id] = luck;
+    }
+
+
 }
