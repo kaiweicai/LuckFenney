b1d787702faa7c6521842eae098e2a8844cb67e5
kaiweicai <kaiweicai@163.com>
2022-08-23 01:07:53 +0800

    add platformToken

--- contracts/PlatfromToken.sol
+++ contracts/PlatfromToken.sol
@@ -3,9 +3,9 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract Pledge is  ERC20{
-
-     constructor(string memory name_, string memory symbol_) ERC20(name_,symbol_) {
-        
-    }
+contract PlatfromToken is ERC20 {
+    
+    constructor(string memory name_, string memory symbol_)
+        ERC20(name_, symbol_)
+    {}
 }
